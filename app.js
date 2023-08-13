d48cd6ae4c05817b7ed6b40d3b615f8a8ec6ccba
Saloni Chauhan <schauhan1@hawk.iit.edu>
2023-08-13 15:28:28 -0500

    all the base for the passport project from starting

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
 const express = require('express');
 const ejs = require('ejs');
 const expressLayouts = require('express-ejs-layouts');
+const mongoose = require('mongoose');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+// DB
+const mongoURI = require('./config/keys').MongoURI;
+
+// Connect to Mongo
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('Connected to MongoDb Atlas'))
+    .catch(err => console.log(err));
+
+// const db = mongoose.connection;
+// db.on('error', console.error.bind(console, 'Connection error'));
+// db.once('open', () => {
+//     console.log('Connected to MongoDb Atlas')
+// });
 
 // EJS
 app.use(expressLayouts);
---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    MongoURI: 'mongodb+srv://saloni2210:test123@testapppassport.pxemohn.mongodb.net/?retryWrites=true&w=majority'
+}
