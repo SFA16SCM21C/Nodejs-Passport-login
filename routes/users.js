498bcfd29b1be5662d16d660b45ef64db87d15d8
Saloni Chauhan <schauhan1@hawk.iit.edu>
2023-08-21 18:47:46 -0500

    adding errrors on post

--- routes/users.js
+++ routes/users.js
@@ -9,8 +9,21 @@ router.get('/register', (req, res) => res.render('register'));
 
 // Register Handle
 router.post('/register', (req, res) => {
-    console.log(req.body);
-    res.send('post request was successful');
+    //console.log(req.body);
+    //res.send('post request was successful');
+
+    const { name, email, password, password2 } = req.body;
+    let errors = [];
+
+    // checking for the required fields to generate errors
+    if (!name || !email || !password || !password2) {
+        errors.push({ msg: 'Please fill in all fields' });
+    }
+
+    // checking if the password mathc or not
+    if (password !== password2) {
+        errors.push({ msg: 'Passwords do not match' });
+    }
 });
 
 module.exports = router;
